e99f3dff1d7309ed297c16ca4da381e1dda0c7d9
Sherif Ismail <sherifismail@proslamx.local>
2017-12-20 14:25:04 -0800

    dispalyed categories relevent to each vehicle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,16 +24,23 @@ $(document).ready(function() {
 
     // let carCategories = [];
 
-    // ['Luxury', 'Sports'] ===> 'Luxury, Sports'
+    // [{name: 'Luxury'}, {name: 'Sports'}] ===> ['Luxury', 'Sports'] ===> 'Luxury, Sports'
     // use .map and .join
+
     
+    
+    var newValues = vehicle.categories.map(function (obj) {
+	    return obj.name;
+	});
+    newValues = newValues.join(', ');
+	    
     let carHTML = 
     `<div class="card" style="width: 20rem;">
       <img class="card-img-top" src=${vehicle.image}> 
         <h3 class="card-title">${vehicle.make}</h3>
         <h4 class="card-text">${vehicle.model}</h4>
         <h5 class="card-text">${vehicle.year}, ${vehicle.color}</h5>
-        <p class="card-text">${vehicle.categories[0].name}, ${vehicle.categories[1].name}</p>
+        <p class="card-text">${newValues}</p>
         <button>Edit</button>
         <button>Delete</button>
       </div>
