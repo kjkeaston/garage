0d011208f6d10ca2f0337cf5669f0bb96403404a
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-20 11:54:40 -0800

    can console seed.js and see cars in console in terminal and FE

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
   console.log('app.js loaded!');
 
 
+  $.ajax({
+    method: 'GET',
+    url: '/api/vehicles',
+    success: onSuccess,
+    error: function getError (data) {
+      console.log("error getting vehicles" + data);
+    }
+  })
+
+  function onSuccess (vehicles) {
+    console.log(vehicles);
+  }
+
+
 
 
 });
